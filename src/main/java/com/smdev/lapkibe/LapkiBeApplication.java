dae41f517df46172e46d94800c32bbb7d4d480ae
ALazyGuy <razorprogrammer@gmail.com>
2021-09-30 01:59:35 +0300

    init: Created and configured project

---
+++ src/main/java/com/smdev/lapkibe/LapkiBeApplication.java
@@ -0,0 +1,13 @@
+package com.smdev.lapkibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapkiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LapkiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smdev/lapkibe/LapkiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smdev.lapkibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapkiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
