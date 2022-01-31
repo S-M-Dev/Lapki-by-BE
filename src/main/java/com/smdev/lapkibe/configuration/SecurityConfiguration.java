56796668108aae52c7daff67ea106eceaf533d89
Pavel Razuvalau <pavel@razuvalov.com>
2022-01-31 03:59:12 +0300

    fix: fix cors error

--- src/main/java/com/smdev/lapkibe/configuration/SecurityConfiguration.java
+++ src/main/java/com/smdev/lapkibe/configuration/SecurityConfiguration.java
@@ -50,6 +50,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+                .antMatchers("/api/user").permitAll()
                     .antMatchers("/api/user/register").permitAll()
                     .antMatchers("/api/user/login").permitAll()
                 .antMatchers("/api/mail/send").permitAll()
